e844c5d8af2bc0249cf455b31c234f4ab197c8b3
Mohamed Afifi <Mhmd-Kamal>
2021-11-02 03:25:37 +0200

    make it better

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -2,13 +2,7 @@
 const router = require('express').Router();
 const productCtrl = require('../controllers/productCtrl');
 
-router
-  .route('/products')
-  .get(productCtrl.getProducts)
-  .post(productCtrl.createProduct);
-router
-  .route('/products/:id')
-  .delete(productCtrl.deleteProduct)
-  .put(productCtrl.updateProduct);
+router.route('/products').get(productCtrl.getProducts).post(productCtrl.createProduct);
+router.route('/products/:id').delete(productCtrl.deleteProduct).put(productCtrl.updateProduct);
 
 module.exports = router;
