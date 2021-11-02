bb066234be550c6db2c15d01af586585ce0482bd
Mohamed Afifi <Mhmd-Kamal>
2021-11-02 04:47:12 +0200

    make it better

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ mongoose.connect(
 );
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
